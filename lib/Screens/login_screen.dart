c5aecf875e40f42c9af01df0634e8b70dd1ed7d0
RizalGusnanda <muchamadrizalgusnanda@gmail.com>
2022-12-17 02:26:25 +0700

    Login and logout

---
+++ lib/Screens/login_screen.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutterapp/Services/auth_services.dart';
+import 'package:flutterapp/Services/globals.dart';
+import 'package:flutterapp/rounded_button.dart';
+import 'package:http/http.dart' as http;
+
+import 'home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailTxt =
+      TextEditingController(text: 'superadmin1@gmail.com');
+  final TextEditingController passwordTxt =
+      TextEditingController(text: 'password');
+  String _email = '';
+  String _password = '';
+  final String _deviceName = 'Mobile';
+
+  loginPressed() async {
+    bool emailValid = RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(emailTxt.text);
+    if (emailValid && passwordTxt != null) {
+      http.Response response = await AuthServices.login(
+          emailTxt.text, passwordTxt.text, _deviceName);
+      Map responseMap = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        print('berhasil');
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => const HomeScreen(),
+            ));
+      } else {
+        errorSnackBar(context, responseMap.values.first);
+      }
+    } else {
+      errorSnackBar(context, 'enter all required fields');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.indigo[300],
+          centerTitle: true,
+          elevation: 0,
+          title: const Text(
+            'Login',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: emailTxt,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your email',
+                ),
+                onChanged: (value) {
+                  _email = value;
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                controller: passwordTxt,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your password',
+                ),
+                onChanged: (value) {
+                  _password = value;
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              RoundedButton(
+                btnText: 'LOG IN',
+                onBtnPressed: loginPressed,
+              )
+            ],
+          ),
+        ));
+  }
+}
